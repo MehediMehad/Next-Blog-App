dafa9f3627183e251a4acf64a6dadb5c879c56b8
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-02-11 13:51:31 +0600

    Client-Side Data Fetching (RTK Query)

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,11 +1,20 @@
+"use client";
 import BlogCard from "@/components/ui/BlogCard";
+import Spinner from "@/components/ui/Spinner";
+import { useGetBlogsQuery } from "@/redux/apis/blogs.slice";
 import { Blog } from "@/types";
 
-const BlogsPage = async () => {
-  const res = await fetch("http://localhost:3000/api/blogs", {
-    cache: "no-store",
-  });
-  const blogs = await res.json();
+const BlogsPage = () => {
+  // const res = await fetch("http://localhost:3000/api/blogs", {
+  //   cache: "no-store",
+  // });
+  // const blogs = await res.json();
+
+  const { data: blogs, isLoading } = useGetBlogsQuery({});
+
+  if (isLoading) {
+    return <Spinner />;
+  }
   return (
     <div className="w-[90%] mx-auto">
       <h1 className="text-3xl text-center my-5 font-bold">
@@ -18,7 +27,7 @@ const BlogsPage = async () => {
         </i>
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {blogs.map((blog: Blog) => (
+        {blogs?.map((blog: Blog) => (
           <BlogCard key={blog.id} blog={blog} />
         ))}
       </div>
